d607b9770ccda48c393c51343c976219997da4b7
Irina Yukavskaya <strix1813@yandex.ru>
2024-03-23 18:28:11 +0300

    update StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -5,6 +5,7 @@ public class StringSchema extends BaseSchema {
         super();
     }
 
+    @Override
     public StringSchema required() {
         addCheck(REQUIRED, object -> object instanceof String string && !string.isEmpty());
         return this;
@@ -14,7 +15,7 @@ public class StringSchema extends BaseSchema {
         if (length >= 0) {
             addCheck(MIN_LENGTH, value -> ((String) value).length() >= length);
         } else {
-            throw new IllegalArgumentException("The argument cannot be less than zero");
+            throw new IllegalArgumentException("The argument cannot be less than 0");
         }
         return this;
     }
