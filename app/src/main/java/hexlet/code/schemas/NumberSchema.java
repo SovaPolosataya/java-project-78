a10658e43a4580d4801d7fb18bf06949eb613077
Irina Yukavskaya <strix1813@yandex.ru>
2024-03-24 16:34:21 +0300

    fixed bugs

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,28 +1,35 @@
 package hexlet.code.schemas;
 
-public class NumberSchema extends BaseSchema {
+public class NumberSchema<T> extends BaseSchema<T> {
+
     public NumberSchema() {
         super();
     }
 
     @Override
-    public NumberSchema required() {
-        addCheck(REQUIRED, object -> object instanceof Integer);
+    public NumberSchema<T> required() {
+        addCheck(REQUIRED,
+                object -> object instanceof Integer);
+
         return this;
     }
 
-    public NumberSchema positive() {
-        addCheck(POSITIVE, value -> value == null || ((Integer) value) > 0);
+    public NumberSchema<T> positive() {
+        addCheck(POSITIVE,
+                value -> value == null || ((Integer) value) > 0);
+
         return this;
     }
 
-    public NumberSchema range(Integer num1, Integer num2) {
+    public NumberSchema<T> range(Integer num1, Integer num2) {
         if (num2 > num1) {
-            addCheck(RANGE, value -> ((Integer) value) >= num1 && ((Integer) value) <= num2);
+            addCheck(RANGE,
+                    value -> ((Integer) value) >= num1 && ((Integer) value) <= num2);
         } else {
             throw new IllegalArgumentException(
                     "Invalid argument values, the first argument cannot be larger than the second.");
         }
+
         return this;
     }
 }
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,27 +1,34 @@
 package hexlet.code.schemas;
 
-public class StringSchema extends BaseSchema {
+public class StringSchema<T> extends BaseSchema<T> {
+
     public StringSchema() {
         super();
     }
 
     @Override
-    public StringSchema required() {
-        addCheck(REQUIRED, object -> object instanceof String string && !string.isEmpty());
+    public StringSchema<T> required() {
+        addCheck(REQUIRED,
+                object -> object instanceof String string && !string.isEmpty());
+
         return this;
     }
 
-    public StringSchema minLength(Integer length) {
+    public StringSchema<T> minLength(Integer length) {
         if (length >= 0) {
-            addCheck(MIN_LENGTH, value -> ((String) value).length() >= length);
+            addCheck(MIN_LENGTH,
+                    value -> ((String) value).length() >= length);
         } else {
             throw new IllegalArgumentException("The argument cannot be less than 0");
         }
+
         return this;
     }
 
-    public StringSchema contains(String substring) {
-        addCheck(CONTAINS, value -> ((String) value).contains(substring));
+    public StringSchema<T> contains(String substring) {
+        addCheck(CONTAINS,
+                value -> ((String) value).contains(substring));
+
         return this;
     }
 }
