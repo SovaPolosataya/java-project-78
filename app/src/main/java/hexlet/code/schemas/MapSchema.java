cf4cb4ee78f7e742c5f795b5988e428d54aaa5a1
Irina Yukavskaya <strix1813@yandex.ru>
2024-03-24 16:33:54 +0300

    add shape()

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,24 +2,39 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public class MapSchema extends BaseSchema {
+public class MapSchema<R, T> extends BaseSchema<T> {
 
     public MapSchema() {
         super();
     }
 
     @Override
-    public MapSchema required() {
-        addCheck(REQUIRED, object -> object instanceof Map);
+    public MapSchema<R, T> required() {
+        addCheck(REQUIRED,
+                object -> object instanceof Map);
+
         return this;
     }
 
-    public MapSchema sizeof(Integer size) {
+    public MapSchema<R, T> sizeof(Integer size) {
         if (size >= 0) {
-            addCheck(SIZE_OF, map -> ((Map) map).size() == size);
+            addCheck(SIZE_OF,
+                    map -> ((Map) map).size() == size);
         } else {
             throw new IllegalArgumentException("The argument cannot be less than 0");
         }
+
+        return this;
+    }
+
+    public  MapSchema<R, T> shape(Map<String, BaseSchema<T>> mapSchema) {
+        addCheck(SHAPE,
+                map -> mapSchema.entrySet()
+                        .stream()
+                        .allMatch(e ->
+                e.getValue().isValid((T) ((Map) map).get(e.getKey())))
+        );
+
         return this;
     }
 }
